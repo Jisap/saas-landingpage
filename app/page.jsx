4c42f0c4ad53cd2b1f78a3b55a1020feedf38587
Jorge Matos <jisapjisap@gmail.com>
2024-09-06 14:39:03 +0200

    brands

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 import Hero from '@/components/Hero'
 
 import { ReactLenis } from 'lenis/dist/lenis-react'
+import Brands from '@/components/Brands'
 
 const Home = () => {
   return (
     <>
       <ReactLenis root>
         <Hero />
+        <Brands />
         <div className='h-[8000px]'></div>
       </ReactLenis>
     </>
