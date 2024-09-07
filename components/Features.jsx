1b8c5bf16272ceed85d845a8bc0a40e5ec261576
Jorge Matos <jisapjisap@gmail.com>
2024-09-07 15:08:55 +0200

    Features init

--- components/Features.jsx
+++ components/Features.jsx
@@ -12,15 +12,76 @@ const featureData = [
     description: "Organize and prioritize task effortlesslu",
     highlights: [
       "Set die dates amd reminders",
+      "Track progress and milestones",
+      "Easily dort tasks reminders",  
     ]
-  }
+  },
+  {
+    imgSrc: "features/img-2.svg",
+    title: "Real-Time Collaboration",
+    description: "Collaborate with your team in real-time",
+    highlights: [
+      "Set die dates amd reminders",
+      "Track progress and milestones",
+      "Easily dort tasks reminders",
+    ]
+  },
+  {
+    imgSrc: "features/img-3.svg",
+    title: "Advanced Analytics",
+    description: "Gains insights to boost productivity",
+    highlights: [
+      "Set die dates amd reminders",
+      "Track progress and milestones",
+      "Easily dort tasks reminders",
+    ]
+  },
+  {
+    imgSrc: "features/img-4.svg",
+    title: "Customizable Workspaces",
+    description: "Personalize your workspace for maximum efficiency",
+    highlights: [
+      "Set die dates amd reminders",
+      "Track progress and milestones",
+      "Easily dort tasks reminders",
+    ]
+  },
 ]
 
 
 const Features = () => {
   return (
-    <section>
-
+    <section className="text-white pt-32 relative">
+      <div className="container mx-auto">
+        <div className="flex gap-16">
+          {/* img */}
+          <div>img</div>
+          {/* text */}
+          <div className="flex-1 flex flex-col gap-24">
+            {featureData.map((item, itemIndex) => {
+              return (
+                <div>
+                  <div>
+                    <h2>{item.title}</h2>
+                    <p>{item.description}</p>
+                    {/* highlights */}
+                    <div className="flex flex-col gap-5">
+                      {item.highlights.map((highlight, index) => {
+                        return (
+                          <div>
+                            <FaRegCheckCircle className="text-accent_secondary text-2xl" />
+                            <p>{highlight}</p>
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
