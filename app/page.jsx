706cc457793832c4414018450fb5849cf3d32099
Jorge Matos <jisapjisap@gmail.com>
2024-09-08 19:36:02 +0200

    Integrations

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,7 @@ import Hero from '@/components/Hero'
 import { ReactLenis } from 'lenis/dist/lenis-react'
 import Brands from '@/components/Brands'
 import Features from '@/components/Features'
+import Integrations from '@/components/Integrations'
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
         <Hero />
         <Brands />
         <Features />
+        <Integrations />
         <div className='h-[8000px]'></div>
       </ReactLenis>
     </>
