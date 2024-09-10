6c52b4e42271b05d4f0aa8ccbd4ca7a495ec157b
Jorge Matos <jisapjisap@gmail.com>
2024-09-10 17:11:30 +0200

    testimonial finish

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -34,75 +34,78 @@ const Testimonial = () => {
     <section className="w-full xl:py-24 mb-24 xl:mb-32 flex justify-center items-center">
       <div className="overflow-hidden">
         {/* text */}
-        <div>
-          <h2 className="h2 mb-6 text-center">What People Are Saying</h2>
+        <motion.div
+          variants={fadeInOnScroll(0.2, 0.4)}
+          initial="hidden"
+          whileInView="visible"
+        >
+          <h2 className="h2 mb-4 text-center">What People Are Saying</h2>
           <p className="lead text-center mb-24">Hear directly from those who have improved their efficiency and organization.</p>
-        </div>
+        </motion.div>
+
         {/* card list */}
-        <div>
+        <motion.div 
+          variants={fadeInOnScroll(0.2, 0.6)}
+          initial="hidden"
+          whileInView="visible"
+          className="flex"
+        >
           <motion.div 
-            variants={fadeInOnScroll(0.2, 0.6)}
-            initial="hidden"
-            whileInView="visible"
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{
+              duration: 30,
+              repeat: Infinity,
+              ease: "linear"
+            }}
             className="flex"
           >
-            <motion.div 
-              initial={{ x: 0 }}
-              animate={{ x: "-100%" }}
-              transition={{
-                duration: 30,
-                repeat: Infinity,
-                ease: "linear"
-              }}
-              className="flex"
-            >
-              {testimonial.map((item, index) => {
-                return (
-                  <div 
-                    key={index} 
-                    className="relative w-[460px] h-[300px] bg-[#0e11354e] mr-12 rounded-2xl flex flex-col justify-center px-14"
-                  >
-                    <BiSolidQuoteLeft className="text-accent mb-3 text-3xl" />
-                    <p className="mb-4 text-lg text-white/80">
-                      {item.message}
-                    </p>
-                    <p className="text-xl">
-                      {item.name}
-                    </p>
-                  </div>
-                )
-              })}
-            </motion.div>
-
-            <motion.div
-              initial={{ x: 0 }}
-              animate={{ x: "-100%" }}
-              transition={{
-                duration: 30,
-                repeat: Infinity,
-                ease: "linear"
-              }}
-              className="flex"
-            >
-              {testimonial.map((item, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="relative w-[460px] h-[300px] bg-[#0e11354e] mr-12 rounded-2xl flex flex-col justify-center px-14"
-                  >
-                    <BiSolidQuoteLeft className="text-accent mb-3 text-3xl" />
-                    <p className="mb-4 text-lg text-white/80">
-                      {item.message}
-                    </p>
-                    <p className="text-xl">
-                      {item.name}
-                    </p>
-                  </div>
-                )
-              })}
-            </motion.div>
+            {testimonial.map((item, index) => {
+              return (
+                <div 
+                  key={index} 
+                  className="relative w-[460px] h-[300px] bg-[#0e11354e] mr-12 rounded-2xl flex flex-col justify-center px-14"
+                >
+                  <BiSolidQuoteLeft className="text-accent mb-3 text-3xl" />
+                  <p className="mb-4 text-lg text-white/80">
+                    {item.message}
+                  </p>
+                  <p className="text-xl">
+                    {item.name}
+                  </p>
+                </div>
+              )
+            })}
           </motion.div>
-        </div>
+
+          <motion.div
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{
+              duration: 30,
+              repeat: Infinity,
+              ease: "linear"
+            }}
+            className="flex"
+          >
+            {testimonial.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="relative w-[460px] h-[300px] bg-[#0e11354e] mr-12 rounded-2xl flex flex-col justify-center px-14"
+                >
+                  <BiSolidQuoteLeft className="text-accent mb-3 text-3xl" />
+                  <p className="mb-4 text-lg text-white/80">
+                    {item.message}
+                  </p>
+                  <p className="text-xl">
+                    {item.name}
+                  </p>
+                </div>
+              )
+            })}
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   )
