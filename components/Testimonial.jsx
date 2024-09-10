11364cff3aaa7da74814d3046ddfd3f879cfd46d
Jorge Matos <jisapjisap@gmail.com>
2024-09-10 17:00:36 +0200

    testimonial text

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -34,7 +34,10 @@ const Testimonial = () => {
     <section className="w-full xl:py-24 mb-24 xl:mb-32 flex justify-center items-center">
       <div className="overflow-hidden">
         {/* text */}
-        <div>text</div>
+        <div>
+          <h2 className="h2 mb-6 text-center">What People Are Saying</h2>
+          <p className="lead text-center mb-24">Hear directly from those who have improved their efficiency and organization.</p>
+        </div>
         {/* card list */}
         <div>
           <motion.div 
