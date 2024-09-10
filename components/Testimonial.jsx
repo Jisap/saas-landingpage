73433c833cbc9b7e95481f37fd24ab59e63c8fc4
Jorge Matos <jisapjisap@gmail.com>
2024-09-10 16:33:38 +0200

    testimonial estructure

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { fadeInOnScroll } from "@/motion/motionVariants"
+import { motion } from "framer-motion"
+import { BiSolidQuoteLeft} from 'react-icons/bi'
+
+const testimonial = [
+  {
+    name: "John Doe",
+    message: "This service was incredible! It exceeded my expectations and I will definitely recommend it to others.",
+  },
+  {
+    name: "Jane Doe",
+    message: "I was impressed with the level of support and responsiveness provided by this company. They truly went above and beyond to make my experience as smooth and enjoyable as possible.",
+  },
+  {
+    name: "Bob Smith",
+    message: "I was blown away by the quality of work and the level of support provided by this company. They truly went above and beyond to make my experience as smooth and enjoyable as possible.",
+  },
+  {
+    name: "Mark Brown",
+    message: "This service was incredible! It exceeded my expectations and I will definitely recommend it to others.",
+  },
+  {
+    name: "Samuel Green",
+    message: "I was impressed with the level of support and responsiveness provided by this company.",
+  },
+  {
+    name: "Crhistiano Rowland",
+    message: "Incredible service! I was blown away by the quality of work and the level of support provided by this company.",
+  },
+]
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <section className="w-full xl:py-24 mb-24 xl:mb-32 flex justify-center items-center">
+      <div className="overflow-hidden">
+        {/* text */}
+        <div>text</div>
+        {/* card list */}
+        <div>
+          <div>
+            <div>
+              {testimonial.map((item, index) => {
+                return (
+                  <div>card</div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
