bf4ce9058ec2b49440feaecce49d91108a5680bc
Jorge Matos <jisapjisap@gmail.com>
2024-09-11 20:17:30 +0200

    trial aniations

--- components/Trial.jsx
+++ components/Trial.jsx
@@ -1,4 +1,4 @@
-import { fadeInSpring } from "@/motion/motionVariants"
+import { fadeInOnScroll, fadeInSpring } from "@/motion/motionVariants"
 import { motion } from "framer-motion"
 import ButtonTertiary from "./ButtonTertiary"
 
@@ -7,8 +7,13 @@ import ButtonTertiary from "./ButtonTertiary"
 const Trial = () => {
   return (
     <section className="xl:mb-32 w-full">
-      <div className="py-24 w-full xl:max-w-[1140px] mx-auto min-h-[300px] bg-gradient-to-r from-accent via-accent_secondary to-accent h-full flex items-center xl:rounded-2xl">
-        <div className="flex flex-col xl:flex-row items-center justify-between w-full xl:px-24">
+      <motion.div 
+        variants={fadeInOnScroll(0.2, 0.6)}
+        initial="hidden"
+        whileInView="visible"
+        className="py-24 w-full xl:max-w-[1140px] mx-auto min-h-[300px] bg-gradient-to-r from-accent via-accent_secondary to-accent h-full flex items-center xl:rounded-2xl">
+        <div className="flex flex-col xl:flex-row items-center justify-between w-full xl:px-24"
+      >
           <div className="">
             {/* text */}
             <div className="text-center mb-12 xl:mb-0 xl:tex-left">
@@ -26,7 +31,7 @@ const Trial = () => {
             <ButtonTertiary btnText="Get started" />
           </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
